40219ca73ed2b42cba70483700ae47ac30d6fff4
jaycaz <jacazamias@gmail.com>
2015-10-14 08:39:11 -0700

    Automatic transition to home screen

--- pages.js
+++ pages.js
@@ -2,10 +2,42 @@
 
 var activePageId = '#page-greeting';
 
+// Move to home page after loading index
+$(document).ready(function(){
+  setTimeout(function(){
+    $('#nav-home').trigger('click');
+  }, 1500);
+});
+
 function resetActiveButton()
 {
   $('.nav-btn.active').removeClass('active');
+  $('body').removeClass('body-scroll');
 }
+
+// function triggerNextButton()
+// {
+//   var activeButton = $('.nav-btn.active')[0];
+//   var buttons = $('.nav-btn');
+//   var activeIndex = $.inArray(activeButton, buttons);
+//
+//   if(activeIndex != -1 && activeIndex < buttons.size()-1) {
+//     resetActiveButton();
+//     buttons.get(activeIndex+1).trigger('click');
+//   }
+// }
+//
+// function triggerPrevButton()
+// {
+//   var activeButton = $('.nav-btn.active');
+//   var buttons = $('.nav-btn');
+//   var activeIndex = $.inArray(activeButton, buttons);
+//
+//   if(activeIndex != -1 && activeIndex > 0) {
+//     resetActiveButton();
+//     buttons.get(activeIndex-1).trigger('click');
+//   }
+// }
 
 function vw()
 {
@@ -42,6 +74,17 @@ $(window).resize(function(){
   snapToPage(activePageId);
 });
 
+// // Keybindings
+// $(document).keydown(function(event){
+//   // console.log('Key code ' + event.which + ' registered.');
+//   if(event.which == 39) { // Right Arrow
+//     triggerNextButton();
+//   }
+//   else if(event.which == 37) { // Right Arrow
+//     triggerPrevButton();
+//   }
+// })
+
 // Callbacks for individual nav buttons
 $('#nav-home').click(function(){
   resetActiveButton();
@@ -72,6 +115,7 @@ $('#nav-projects').click(function(){
   $(this).addClass('active');
   moveToPage('#page-projects');
   activePageId = '#page-projects';
+  $('body').addClass('body-scroll');
 
 });
 
--- projects.js
+++ projects.js
@@ -1,1 +1,11 @@
 // projects.js: Controls behavior for projects page
+
+// var MOVE_FACTOR = '10px';
+//
+// // Toggle size animation when mouse over
+// $('.project-card').mouseenter(function(){
+//   // $(this).animate({marginTop: '-=' + MOVE_FACTOR}, 200)
+//          $(this).animate({paddingBottom: '+=' + MOVE_FACTOR}, 200)})
+//                   .mouseleave(function(){
+//   //$(this).animate({marginTop: '+=' + MOVE_FACTOR}, 200)
+//          $(this).animate({paddingBottom: '-=' + MOVE_FACTOR}, 200)});
